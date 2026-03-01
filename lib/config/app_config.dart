b2192cefa43df82aeeffea77e169195732c3e015
Kevin Pedraza <kevinpedraza2003@gmail.com>
2026-02-28 22:08:44 -0500

    fixed: api places google

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -4,7 +4,7 @@ class AppConfig {
   static const String renderBackendBaseUrl = "https://sasper.onrender.com/api";
   static const String supabaseUrl = 'https://flyqlrujavwndmdqaldr.supabase.co';
   static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZseXFscnVqYXZ3bmRtZHFhbGRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2NDQyOTEsImV4cCI6MjA2NzIyMDI5MX0.gv47_mKVpXRETdHxDC2vGxuOlKK_bgjZz2zqpJMxDXs';
-  static const String googlePlacesApiKey = "AIzaSyBFJi2UL13reVFEqVSnSuGEhbffsyQPyxw";
+  static const String googlePlacesApiKey = "AIzaSyAJOp3ne52VGO2XSn6DfT0S_XyUIorrlVU";
 
   static void checkKeys() {
     if (kDebugMode) {
